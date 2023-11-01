4286e00022ce595bff83162d76350524bbf596c5
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-11-01 09:35:47 +0600

    created the basic app setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='w-full min-h-screen bg-slate-900 text-white'>
+			<h1>Redux is cooking</h1>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+export { default as portalLogo } from './image/learningportal.svg';
+export { default as lwsLogo } from './image/lws.svg';
+export { default as searchImg } from './image/search.svg';
