6c08e56a1597d30db4f387568d7c48dad4b9c1b9
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-11-02 13:54:09 +0600

    set up redux store and api slice

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import { Navbar } from './shared';
 function App() {
 	// get path
 	const location = useLocation();
+
 	return (
 		<>
+			{/* Don't render Navbar if user is not logged in */}
 			{location.pathname !== '/' &&
 				location.pathname !== '/register' &&
 				location.pathname !== '/admin' && <Navbar />}
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import routes from './routes/routes.jsx';
+import store from './app/store.js';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<RouterProvider router={routes}>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</RouterProvider>
 	</React.StrictMode>
 );
